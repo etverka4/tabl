1d7f41515193422446676d51d7b9caa997dc476d
etverka4 <80982731+etverka4@users.noreply.github.com>
2021-09-12 17:57:31 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 import (
     "database/sql"
     "fmt"
-    "github.com/jackc/pgx/v4"
 )
-
- 
 func main() { 
     db, err := sql.Open("pgx", "user=mark password=******** host=localhost port=5432 database=core sslmode=disable")
     if err != nil {
